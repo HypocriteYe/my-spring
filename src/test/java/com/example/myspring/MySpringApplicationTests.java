7c73afcdd1bd1f55c4c65652873571fe1098f644
ygl <934752370@qq.com>
2023-10-12 17:19:54 +0800

    test:v3.0

--- src/test/java/com/example/myspring/MySpringApplicationTests.java
+++ src/test/java/com/example/myspring/MySpringApplicationTests.java
@@ -18,22 +18,22 @@ class MySpringApplicationTests {
 //    @Autowired
 //    YamlPropertiesFactoryBean yamlPropertiesFactoryBean;
 
-    @Test
-    void contextLoads() {
-        //        stringRedisTemplate.opsForValue().setIfAbsent("key", "value");
-        stringRedisTemplate.opsForValue().set("key3","value3",60, TimeUnit.MINUTES);
-        String s = stringRedisTemplate.opsForValue().get("key3");
-        System.out.println(s);
-        System.out.println(stringRedisTemplate.getExpire("key3"));
-        stringRedisTemplate.expire("key3", 0, TimeUnit.SECONDS);
-        System.out.println(stringRedisTemplate.getExpire("key3"));
-    }
-
-    @Test
-    void testConvert() {
-        YamlPropertiesFactoryBean yamlPropertiesFactoryBean = new YamlPropertiesFactoryBean();
-        Properties properties = yamlPropertiesFactoryBean.getObject();
-        System.out.println(properties);
-    }
+//    @Test
+//    void contextLoads() {
+//        //        stringRedisTemplate.opsForValue().setIfAbsent("key", "value");
+//        stringRedisTemplate.opsForValue().set("key3","value3",60, TimeUnit.MINUTES);
+//        String s = stringRedisTemplate.opsForValue().get("key3");
+//        System.out.println(s);
+//        System.out.println(stringRedisTemplate.getExpire("key3"));
+//        stringRedisTemplate.expire("key3", 0, TimeUnit.SECONDS);
+//        System.out.println(stringRedisTemplate.getExpire("key3"));
+//    }
+//
+//    @Test
+//    void testConvert() {
+//        YamlPropertiesFactoryBean yamlPropertiesFactoryBean = new YamlPropertiesFactoryBean();
+//        Properties properties = yamlPropertiesFactoryBean.getObject();
+//        System.out.println(properties);
+//    }
 
 }
