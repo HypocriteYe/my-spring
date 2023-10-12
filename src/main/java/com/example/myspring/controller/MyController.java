99251b91c3fa497ff1a2ef6346dd97c7deee6d10
ygl <934752370@qq.com>
2023-10-12 14:14:54 +0800

    test:v1.0

---
+++ src/main/java/com/example/myspring/controller/MyController.java
@@ -0,0 +1,35 @@
+package com.example.myspring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @description
+ * @Author ygl
+ * @Create 2023/10/9 10:01 AM
+ */
+@RestController
+public class MyController {
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    @GetMapping("/testHost")
+    public String testHost(HttpServletRequest request) {
+
+        String remoteHost = request.getRemoteHost();
+        return remoteHost;
+    }
+
+    @GetMapping("/hello")
+    public String count() {
+        Long increment = redisTemplate.opsForValue().increment("docker-click-test");
+        return String.valueOf(increment);
+
+    }
+}
---
+++ src/test/java/com/example/myspring/MySpringApplicationTests.java
@@ -0,0 +1,39 @@
+package com.example.myspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest
+class MySpringApplicationTests {
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+//    @Autowired
+//    YamlPropertiesFactoryBean yamlPropertiesFactoryBean;
+
+    @Test
+    void contextLoads() {
+        //        stringRedisTemplate.opsForValue().setIfAbsent("key", "value");
+        stringRedisTemplate.opsForValue().set("key3","value3",60, TimeUnit.MINUTES);
+        String s = stringRedisTemplate.opsForValue().get("key3");
+        System.out.println(s);
+        System.out.println(stringRedisTemplate.getExpire("key3"));
+        stringRedisTemplate.expire("key3", 0, TimeUnit.SECONDS);
+        System.out.println(stringRedisTemplate.getExpire("key3"));
+    }
+
+    @Test
+    void testConvert() {
+        YamlPropertiesFactoryBean yamlPropertiesFactoryBean = new YamlPropertiesFactoryBean();
+        Properties properties = yamlPropertiesFactoryBean.getObject();
+        System.out.println(properties);
+    }
+
+}
